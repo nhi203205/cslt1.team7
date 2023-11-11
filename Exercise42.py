1a8e1319d279fb144c940f2e2d4903b6d2950978
Nguyen Thi Thao Nhi <146497353+nhi203205@users.noreply.github.com>
2023-11-11 09:50:20 +0700

    Update Exercise42.py

--- Exercise42.py
+++ Exercise42.py
@@ -1,17 +1,17 @@
 frequency=float(input("Enter the frequency: "))
 if abs(frequency - 261.63) <= 1:
-    print("Name note is C")
+    print("Name note is C4")
 elif abs(frequency - 293.66) <= 1:
-    print("Name note is D")
+    print("Name note is D4")
 elif abs(frequency - 329.63) <= 1:
-    print("Name note is E")
+    print("Name note is E4")
 elif abs(frequency - 349.23) <= 1:
-    print("Name note is F")
+    print("Name note is F4")
 elif abs(frequency - 392.00) <= 1:
-    print("Name note is G")
+    print("Name note is G4")
 elif abs(frequency - 440.00) <= 1:
-    print("Name note is A")
+    print("Name note is A4")
 elif abs(frequency - 493.88) <= 1:
-    print("Name note is B")
+    print("Name note is B4")
 else:
     print("The frequency does not correspond to a known note.")
